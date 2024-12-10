0e1a592cbb71ae943012c7b2f849317bc72fbe94
johnbosco35 <okonkwovincent63@gmail.com>
2024-12-10 22:14:59 +0100

    added

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world")
+import express, { Application } from "express"
+
+
+const app: Application = express();
+
+const port: Number = 5000;
+
+const server = app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})
+
+process.on("unhandledRejection", (error: any) =>{
+    console.log("unhandledRejection",error);
+    process.exit(1);
+});
+
+process.on("uncaughtException", (reason:any)=>{
+    console.log("uncaughtException",reason);
+
+    server.close(() =>{
+        process.exit(1);
+    });
+});

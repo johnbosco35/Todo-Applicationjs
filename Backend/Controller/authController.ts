4bbe41b415e0a6617f7deafef70d0adc6959a17b
johnbosco35 <okonkwovincent63@gmail.com>
2024-12-18 00:04:48 +0100

    Almost done with authController.ts

--- Backend/Controller/authController.ts
+++ Backend/Controller/authController.ts
@@ -28,3 +28,51 @@ export const register = async (req:Request, res:Response) => {
         })
     }
 }
+
+export const login = async (req:Request, res:Response) => {
+    const { email, password } = req.body
+    const user = await UserModel.findOne({ email })
+
+    if (!user || ! (await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({
+            message: "Invalid credentials"
+        });
+    }
+    
+    res.json({
+        token: generateToken(user.id)
+    });
+}
+
+export const getAllUser = async (req:Request, res:Response) => {
+    try {
+        const allUser = await UserModel.find()
+
+        res.status(200).json({
+            message: "All users retrieved successfully",
+            data: allUser,
+        })
+    } catch (error:any) {
+        res.status(400).json({
+            message: "Error retrieving all users",
+        })
+    }
+}
+
+export const getOneUser = async (req:Request, res:Response) =>{
+    try {
+        const {userID} = req.params
+
+        const user = await UserModel.findById(userID)
+
+        return res.status(200).json({
+            message: "User retrieved successfully",
+            data: user,
+        })
+    } catch (error:any) {
+        res.status(400).json({
+            message: "Error retrieving user",
+            data: error.message,
+        })
+    }
+}

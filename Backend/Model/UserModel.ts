78d1e84910d128e08658e5def948a1127930cc9a
johnbosco35 <okonkwovincent63@gmail.com>
2024-12-17 04:54:39 +0100

    done with UserModel.ts

---
+++ Backend/Model/UserModel.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { user } from "../util/interfaces";
+
+
+interface IUser extends user, mongoose.Document {}
+
+const UserSchema = new mongoose.Schema<user>(
+    {
+        name: { 
+            type: String, 
+            required: true 
+            },
+            email: {
+                type: String,
+                required: true,
+                unique: true,
+                trim: true,
+                lowercase: true,
+                match: [
+                    /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+                    "Please enter a valid email address.",
+                  ],
+            },
+            password: {
+                type: String,
+                required: true,
+                trim: true,
+
+            }
+            
+    },
+
+    {timestamps: true}
+)
+
+export default mongoose.model("User", UserSchema);

eeb0b47b0d0e84e9fcac7fff9a4e2a8044de1f4c
johnbosco35 <okonkwovincent63@gmail.com>
2024-12-18 23:27:32 +0100

    Almost done with todoRouter.ts

---
+++ Backend/router/todoRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { protect } from "../middleware/authMiddleWare";
+import { createTodo, deleteTodo, getTodos, updateTodo } from "../Controller/todoController";
+
+
+const router = Router();
+
+router.use(protect);
+
+router.get("/get-todos", getTodos);
+router.post("/createTask", createTodo);
+router.put("/updateTask/:id", updateTodo);
+router.delete("/deleteTask/:id", deleteTodo);
+
+export default router;
